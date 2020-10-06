909835b9962f53323ebf2e5ccbe5764fc96b54b0
nida2004 <nyilmaz2025@gmail.com>
2020-10-06 22:16:35 +0200

    adde more awesomert

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,12 @@
+public class AwesomeTest {
+
+    //newdsd
+    //new change
+
+    //I just new change  from github that i didnot have locally
+    //and now my projejt new
+
+    //Now I made new change
+    ///dfdf
+
+}
